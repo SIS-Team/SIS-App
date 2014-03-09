0687ec0a02110289e76b2e9bb23899cbdc8caef8
Hiiias <mx5595@gmail.com>
2014-03-09 23:11:26 +0100

    alternativStundenplan completed/ Popup.js for alterativStundenplan

--- js/popup.js
+++ js/popup.js
@@ -5,27 +5,68 @@ $( document ).ready(function() {
 
 			var info = "";
 			document.getElementById("popupID").innerHTML = info;
+			var substitudeID = document.getElementById(id).getAttribute("data-substitude");
 
-			for(aLesson in timetObject){
+			if(substitudeID == " " || !document.getElementById(id).getAttribute("data-substitude"))	//Falls in dem selbst bestimmten Attribut substitude nur ein Leerzeichen steht werden die Daten aus dem normalen Stundenplanobjekt entnommen
+			{	
+				for(aLesson in timetObject){
 
-				if(timetObject[aLesson].cellid.indexOf(id + "/") != -1)
-				{	
-					if(actualClass != 0)
-						var infoPart = timetObject[aLesson].teShort;
-					else
-						var infoPart = timetObject[aLesson].clName;
-					/*alert(object[aLesson].suShort);*/
+					if(timetObject[aLesson].cellid.indexOf(id + "/") != -1)		//Überprüfen ob die ID die beim Stundenplan erzeugen für dieses Objekt gesetzt wurde mit der ID der gewünschten Zelle übereinstimmt
+					{	
+						
+						if(actualClass != 0)
+							var infoPart = timetObject[aLesson].teShort;	//Wenn es sich um einen Schüler handelt(Klasse ist gesetzt) wird der Lehrer angezeigt
+						else
+							var infoPart = timetObject[aLesson].clName;		//Wenn es sich um einen Leherer handelt(keine Klasse ist gesetzt) wird die Klasse angezeigt
+						/*alert(object[aLesson].suShort);*/
 
-					var infoNew = "<b>" + timetObject[aLesson].suShort + "</b> <br>" + infoPart + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + timetObject[aLesson].roName;
-					if(document.getElementById("popupID").innerHTML != "")
-						var info = document.getElementById("popupID").innerHTML + "<br>" + infoNew;
-					else
-						var info = infoNew;
+						var infoNew = "<b>" + timetObject[aLesson].suShort + "</b> <br>" + infoPart + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + timetObject[aLesson].roName;	//Alle Informationen des PopUps werden zusammengesetzt(Fach + Lehrer/Klasse + Raum)
+						if(document.getElementById("popupID").innerHTML != "")
+							var info = document.getElementById("popupID").innerHTML + "<br>" + infoNew;	//Falls schon Informationen bestehen werden die neuen Informationen hinten angehängt
+						else
+							var info = infoNew;
+					
 
-					document.getElementById("popupID").innerHTML = info;		
+						document.getElementById("popupID").innerHTML = info;		
+					}
 				}
 			}
+			else
+			{
+				for(aLesson in timetObject){
 
+					if(timetObject[aLesson].cellid.indexOf(id + "/") != -1)		//Überprüfen ob die ID die beim Stundenplan erzeugen für dieses Objekt gesetzt wurde mit der ID der gewünschten Zelle übereinstimmt
+					{	
+						
+						if(actualClass != 0){
+							if(timetObject[aLesson].teShort != subsObject[substitudeID].oldTeShort)
+								var infoPart = timetObject[aLesson].teShort;	//Wenn es sich um einen Schüler handelt(Klasse ist gesetzt) wird der Lehrer angezeigt
+							else
+								var infoPart = subsObject[substitudeID].teShort;
+						}
+						else{
+							if(timetObject[aLesson].clName != subsObject[substitudeID].oldClName)
+								var infoPart = timetObject[aLesson].clName;		//Wenn es sich um einen Leherer handelt(keine Klasse ist gesetzt) wird die Klasse angezeigt
+							else
+								var infoPart = subsObject[substitudeID].clName;
+						}
+
+						if(timetObject[aLesson].suShort != subsObject[substitudeID].oldSuShort)
+							var infoNew = "<b>" + timetObject[aLesson].suShort + "</b> <br>" + infoPart + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + timetObject[aLesson].roName;	//Alle Informationen des PopUps werden zusammengesetzt(Fach + Lehrer/Klasse + Raum)
+						else
+							var infoNew = "<b>" + subsObject[substitudeID].suShort + "</b> <br>" + infoPart + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + subsObject[substitudeID].roName;
+
+						if(document.getElementById("popupID").innerHTML != "")
+							var info = document.getElementById("popupID").innerHTML + "<br>" + infoNew;	//Falls schon Informationen bestehen werden die neuen Informationen hinten angehängt
+						else
+							var info = infoNew;
+					
+
+						document.getElementById("popupID").innerHTML = info;		
+					}
+				}
+
+			}
 		    $('.Popup').fadeIn("slow");
 		    $('#overlay').fadeIn("slow");
 		    $('body').css('overflow', 'hidden');
